50aced8bb2face423f1b7b323eebe8647e8a6eae
HT6FFAFL <hamza-externe.tariq@enedis.fr>
2026-08-31 10:43:48 +0200

    feat: update architecture.component

--- src/app/components/pages/architecture/architecture.component.ts
+++ src/app/components/pages/architecture/architecture.component.ts
@@ -1,15 +1,21 @@
-import {Component, inject} from '@angular/core';
-import {MarkdownComponent} from 'ngx-markdown';
-import {TranslateService} from '@ngx-translate/core';
+import { afterNextRender, Component, inject} from '@angular/core';
+import { TranslatePipe, TranslateService} from '@ngx-translate/core';
+import mermaid from 'mermaid';
 
 @Component({
   selector: 'app-architecture',
-  imports: [
-    MarkdownComponent
-  ],
+  imports: [TranslatePipe],
   templateUrl: './architecture.component.html',
   styleUrls: ['./architecture.component.scss'],
 })
 export class ArchitectureComponent {
   protected readonly translate = inject(TranslateService);
+
+  constructor() {
+    afterNextRender({
+      read: () => {
+        void mermaid.run();
+      },
+    });
+  }
 }

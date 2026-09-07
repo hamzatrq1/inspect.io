459cb0b16a66c930676251c61655df39bc4759a3
HT6FFAFL <hamza-externe.tariq@enedis.fr>
2026-09-07 18:19:06 +0200

    chore: removing components not necessary

--- src/app/components/pages/components/sessions/sessions.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  imports: [],
-  selector: 'app-sessions.component',
-  styleUrl: './sessions.component.scss',
-  templateUrl: './sessions.component.html',
-})
-export class SessionsComponent {}

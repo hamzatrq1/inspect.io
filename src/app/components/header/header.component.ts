3c554c29ebbdd5a819336d14821fee118ea6c57e
HT6FFAFL <hamza-externe.tariq@enedis.fr>
2026-09-15 17:20:53 +0200

    feat(darkmode): create a darkmode

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, inject, Output, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { ThemeService } from '@app/services/theme.service';
 
 @Component({
   selector: 'app-header',
@@ -11,12 +12,14 @@ import { TranslateModule, TranslateService } from '@ngx-translate/core';
 })
 export class HeaderComponent {
   private readonly translate = inject(TranslateService);
+  readonly themeService = inject(ThemeService);
 
   @Output() searchOpen = new EventEmitter<void>();
 
   readonly isFirefox = typeof navigator !== 'undefined' && /Firefox\//.test(navigator.userAgent);
   readonly repoMenuOpen = signal(false);
   readonly currentLang = signal(this.translate.currentLang || this.translate.defaultLang || 'en');
+  readonly isDark = this.themeService.isDark;
 
   // Redirect to the latest version of each component on Docker Hub, Maven Central, and npm
   readonly menuItems = [
